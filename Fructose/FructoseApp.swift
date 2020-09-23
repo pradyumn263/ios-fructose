fc03aa25e434c755a20eb626598465ab49ceb8de
Pradyumn Shukla <ps811876@student.nitw.ac.in>
2020-09-23 09:50:00 +0530

    Initial Commit

---
+++ Fructose/FructoseApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructoseApp.swift
+//  Fructose
+//
+//  Created by Pradyumn Shukla on 23/09/20.
+//
+
+import SwiftUI
+
+@main
+struct FructoseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
